989fd78c2856d05422a5b987dfd2617b22fd1b02
jamaludeenabdulnasi-coder <jamaludeenabdulnasir@gmail.com>
2026-03-30 22:16:43 +0000

    first upload

---
+++ index.js
@@ -0,0 +1,81 @@
+function toggleDropdown8() {
+  const content = document.getElementById("dropdown-content8");
+  content.classList.toggle("hidden8");
+}
+
+
+
+
+
+    
+function toggleDropdown() {
+  const content = document.getElementById("dropdown-content");
+  content.classList.toggle("hidden");
+}
+
+
+
+
+function toggleDropdown1() {
+  const content = document.getElementById("dropdown-content1");
+  content.classList.toggle("hidden1");
+}
+
+
+
+
+
+function toggleDropdown2() {
+  const content = document.getElementById("dropdown-content2");
+  content.classList.toggle("hidden2");
+}
+
+
+
+
+function toggleDropdown3() {
+  const content = document.getElementById("dropdown-content3");
+  content.classList.toggle("hidden3");
+}
+
+
+
+
+
+function toggleDropdown4() {
+  const content = document.getElementById("dropdown-content4");
+  content.classList.toggle("hidden4");
+}
+
+
+
+
+
+
+
+function toggleDropdown5() {
+  const content = document.getElementById("dropdown-content5");
+  content.classList.toggle("hidden5");
+}
+
+
+
+
+
+
+
+function toggleDropdown6() {
+  const content = document.getElementById("dropdown-content6");
+  content.classList.toggle("hidden6");
+}
+
+
+
+
+
+
+
+function toggleDropdown7() {
+  const content = document.getElementById("dropdown-content7");
+  content.classList.toggle("hidden7");
+}
